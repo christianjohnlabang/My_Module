53141fc6c674cafc128c48b89bdf4c2b75efa922
christian <labang74@gmail.com>
2016-07-14 19:15:34 +0800

    My_Module

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+const util = require('./mymodule');
+console.log('\n');
+var area =util.computeArea(10,50);
+console.log("Area is:" + area);
+
+console.log('\n');
+
+var perimeter=util.computePerimeter(10,50);
+console.log("Perimeter is:"+perimeter);
